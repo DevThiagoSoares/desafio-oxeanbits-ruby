4c617d51d6f417d1f0b6157bb93bba2c43efff9f
Thiago <devthiagosoares@gmail.com>
2024-03-04 14:39:39 -0400

    feat: adicionando um controller para receber multiplos movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,6 +22,25 @@ class MoviesController < ApplicationController
     end
   end
 
+  def create_multiple
+    movies_params = JSON.parse(params[:movies])["movies"].map { |movie| ActionController::Parameters.new(movie).permit(:title, :director) }
+
+    created_movies = []
+    movies_params.each do |movie_params|
+      movie = Movie.new(movie_params)
+      if movie.save
+        created_movies << movie
+      end
+    end
+
+    if created_movies.any?
+      redirect_to movies_path, notice: "Movies were successfully created."
+    else
+      render json: { error: "Failed to create movies." }, status: :unprocessable_entity
+    end
+  end
+  
+  
   private
 
   def movie_params
